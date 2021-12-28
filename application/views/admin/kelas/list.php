09ab8eac04dccae9384dbf6d74d8edd4e5024045
algamhr <algamahargarika9@gmail.com>
2021-12-28 21:53:22 +0700

    Fix View Kelas For Guru

--- application/views/admin/kelas/list.php
+++ application/views/admin/kelas/list.php
@@ -29,43 +29,58 @@
                              <th>Aksi</th>
                          </tr>
                      </thead>
-                     <?php if($this->session->userdata('akses_level')==21) { ?>
-                        <tbody>
-                         <?php foreach ($kelas as $kelas) { ?>
-                             <tr>
-                                 <td><?php echo $kelas->nama_kelas; ?></td>
-                                 <td>
-                                     <a href="<?php echo base_url('users_kelas/pengguna/' . $kelas->id); ?>" class="btn btn-success btn-sm"><i class="fa fa-users"></i> Murid</a>
-                                     <?php if ($this->session->userdata('akses_level') == '21') { ?>
-                                         <?php include('edit.php'); ?>
-                                         <?php include('delete.php'); ?>
-                                     <?php } ?>
-                                 </td>
-                             </tr>
-                         <?php } ?>
-                     </tbody>
-                    <?php } else { ?>
-                        <tbody>
-                         <?php foreach ($kelas as $kelas) { ?>
-                            <?php if(
-                                $kelas->slug_kelas==$user->kelas1a || 
-                                $kelas->slug_kelas==$user->kelas2a ||
-                                $kelas->slug_kelas==$user->kelas3a 
-                                ){ ?>
-                             <tr>
-                                 <td><?php echo $kelas->nama_kelas; ?></td>
-                                 <td>
-                                     <a href="<?php echo base_url('users_kelas/pengguna/' . $kelas->id); ?>" class="btn btn-success btn-sm"><i class="fa fa-users"></i> Murid</a>
-                                     <?php if ($this->session->userdata('akses_level') == '21') { ?>
-                                         <?php include('edit.php'); ?>
-                                         <?php include('delete.php'); ?>
-                                     <?php } ?>
-                                 </td>
-                             </tr>
+                     <?php if ($this->session->userdata('akses_level') == 21) { ?>
+                         <tbody>
+                             <?php foreach ($kelas as $kelas) { ?>
+                                 <tr>
+                                     <td><?php echo $kelas->nama_kelas; ?></td>
+                                     <td>
+                                         <a href="<?php echo base_url('users_kelas/pengguna/' . $kelas->id); ?>" class="btn btn-success btn-sm"><i class="fa fa-users"></i> Murid</a>
+                                         <?php if ($this->session->userdata('akses_level') == '21') { ?>
+                                             <?php include('edit.php'); ?>
+                                             <?php include('delete.php'); ?>
+                                         <?php } ?>
+                                     </td>
+                                 </tr>
                              <?php } ?>
-                         <?php } ?>
-                     </tbody>
-                    <?php } ?>
+                         </tbody>
+                     <?php } else { ?>
+                         <tbody>
+                             <?php foreach ($kelas as $kelas) { ?>
+                                 <?php if (
+                                        $kelas->slug_kelas == $user->kelas1a ||
+                                        $kelas->slug_kelas == $user->kelas2a ||
+                                        $kelas->slug_kelas == $user->kelas3a ||
+                                        $kelas->slug_kelas == $user->kelas4a ||
+                                        $kelas->slug_kelas == $user->kelas5a ||
+                                        $kelas->slug_kelas == $user->kelas6a ||
+                                        $kelas->slug_kelas == $user->kelas1b ||
+                                        $kelas->slug_kelas == $user->kelas2b ||
+                                        $kelas->slug_kelas == $user->kelas3b ||
+                                        $kelas->slug_kelas == $user->kelas4b ||
+                                        $kelas->slug_kelas == $user->kelas5b ||
+                                        $kelas->slug_kelas == $user->kelas6b ||
+                                        $kelas->slug_kelas == $user->kelas1c ||
+                                        $kelas->slug_kelas == $user->kelas2c ||
+                                        $kelas->slug_kelas == $user->kelas3c ||
+                                        $kelas->slug_kelas == $user->kelas4c ||
+                                        $kelas->slug_kelas == $user->kelas5c ||
+                                        $kelas->slug_kelas == $user->kelas6c
+                                    ) { ?>
+                                     <tr>
+                                         <td><?php echo $kelas->nama_kelas; ?></td>
+                                         <td>
+                                             <a href="<?php echo base_url('users_kelas/pengguna/' . $kelas->id); ?>" class="btn btn-success btn-sm"><i class="fa fa-users"></i> Murid</a>
+                                             <?php if ($this->session->userdata('akses_level') == '21') { ?>
+                                                 <?php include('edit.php'); ?>
+                                                 <?php include('delete.php'); ?>
+                                             <?php } ?>
+                                         </td>
+                                     </tr>
+                                 <?php } ?>
+                             <?php } ?>
+                         </tbody>
+                     <?php } ?>
                  </table>
              </div>
          </div>
